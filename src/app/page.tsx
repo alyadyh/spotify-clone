f65a13691c9f3105176bf9be1c752f661c56adef
Alya Dhiya <alya.mdyh@gmail.com>
2025-09-12 21:51:19 +0700

    fix: adding navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+
 export default function Home() {
   return (
-    <h1>Hello world.</h1>
+    <div className="min-h-screen">
+      <Navbar />
+    </div>
   );
 }
