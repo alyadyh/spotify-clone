fb4f269e59247c6e2fe870aea7b408994c7b042b
Alya Dhiya <alya.mdyh@gmail.com>
2025-09-13 19:11:44 +0700

    fix: adding all songs card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AllSongs from "@/components/AllSongs";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <Navbar />
       <main>
         <Sidebar />
+        <AllSongs />
       </main>
     </div>
   );
---
+++ src/components/AllSongs.tsx
@@ -0,0 +1,7 @@
+export default function AllSongs() {
+  return (
+    <div className="min-h-[90vh] bg-background my-15 p-4 lg:ml-80 rounded-lg mx-4">
+      <h2 className="text-2xl text-white mb-1 font-semibold">New Songs</h2>
+    </div>
+  );
+}
